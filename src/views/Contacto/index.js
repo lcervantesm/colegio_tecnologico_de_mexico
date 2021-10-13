831bce17228f91677ea75623a8474b87575804a5
lcervantesm <cervants2802@gmail.com>
2021-10-12 19:48:30 -0500

    Arreglado ListGroup

--- src/views/Contacto/index.js
+++ src/views/Contacto/index.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/Listgroup';
+import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListgroupItem';
 import Nav from 'react-bootstrap/Nav';
 import ContactForm from '../../Components/ContactForm';
